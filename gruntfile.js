b0b7c721c92507a64b80c7dcfd076486fa376646
ybyao <ybyao@iflytek.com>
2019-08-05 21:18:42 +0800

    decorate 3

--- gruntfile.js
+++ gruntfile.js
@@ -1,39 +1,73 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      dist: {
-        src: [
-          'css/base.css',
-          'css/syntax/emacs.css',
-          'css/octicons.css',
-          'css/font-awesome.min.css'
-        ],
-        dest: 'css/<%= pkg.name %>.add.css'
-      }
-    },
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            main: {
+                src: 'js/<%= pkg.name %>.js',
+                dest: 'js/<%= pkg.name %>.min.js'
+            }
+        },
+        less: {
+            expanded: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {
+                    "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
+                }
+            },
+            minified: {
+                options: {
+                    paths: ["css"],
+                    cleancss: true
+                },
+                files: {
+                    "css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less"
+                }
+            }
+        },
+        banner: '/*!\n' +
+            ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' */\n',
+        usebanner: {
+            dist: {
+                options: {
+                    position: 'top',
+                    banner: '<%= banner %>'
+                },
+                files: {
+                    src: ['css/<%= pkg.name %>.css', 'css/<%= pkg.name %>.min.css', 'js/<%= pkg.name %>.min.js']
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['js/<%= pkg.name %>.js'],
+                tasks: ['uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            less: {
+                files: ['less/*.less'],
+                tasks: ['less'],
+                options: {
+                    spawn: false,
+                }
+            },
+        },
+    });
 
-    cssmin: {
-      'css/main.min.css': [
-        '<%= concat.dist.dest %>'
-      ],
-      'css/main.min.rtl.css': [
-        '<%= rtlcss.rtl.dest %>'
-      ]
-    },
+    // Load the plugins.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-banner');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    rtlcss: {
-      rtl: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'css/<%= pkg.name %>.add.rtl.css'
-      }
-    }
-  });
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'less', 'usebanner']);
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-rtlcss');
-  grunt.registerTask('default', ['concat', 'rtlcss', 'cssmin']);
-  grunt.registerTask('css', ['concat', 'rtlcss', 'cssmin']);
 };
